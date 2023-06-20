2e81acba72d0c3239cd3ffcaee75fdc1879810fb
Smith <smith.dbritto8@gmail.com>
2023-06-20 15:40:30 +0530

    build changed to public in app.js backend

--- backend/app.js
+++ backend/app.js
@@ -32,7 +32,7 @@ app.use("/api/v1", user);
 app.use("/api/v1", order);
 app.use("/api/v1", payment);
 
-app.use(express.static(path.join(__dirname, "../frontend/build")));
+app.use(express.static(path.join(__dirname, "../frontend/public")));
 
 app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "../frontend/public/index.html"));
