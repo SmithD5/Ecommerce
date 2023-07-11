1f00bfb6d35b9830e0cc3ef41c5bf970987c4823
Smith <smith.dbritto8@gmail.com>
2023-07-11 10:45:04 +0530

    Footer.js changed a bit

--- frontend/src/component/layout/Footer/Footer.js
+++ frontend/src/component/layout/Footer/Footer.js
@@ -14,15 +14,14 @@ const Footer = () => {
         </div>
         <div className="midFooter">
             <h1>Ecommerce.</h1>
-            <p>High Quality is our first priority</p>
             <p>All the images belong to their respective owners.</p>
-            <p>Copyright 2023 &copy; SmithDbritto</p>
+            <p>Made by Smith Dbritto</p>
         </div>
         <div className="rightFooter">
             <h4>Follow us</h4>
-            <a href="link">LinkedIn</a>
-            <a href="link">Gmail</a>
-            <a href="link">GitHub</a>
+            <a href="https://www.linkedin.com/in/smith--dbritto/">LinkedIn</a>
+            <a href="https://mail.google.com/mail/?view=cm&fs=1&to=smith.dbritto8@gmail.com">Gmail</a>
+            <a href="https://github.com/SmithD5">GitHub</a>
         </div>
     </footer>
   )
